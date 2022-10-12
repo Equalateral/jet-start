e4d7b5442d67313c0947b042847c1c2ce75d393a
Equalateral <91502737+Equalateral@users.noreply.github.com>
2022-10-12 13:06:11 -0500

    Update data.js
    Changed view.parse --> view sync and handling the promise using data.waitData.then()
    
    https://webix.gitbook.io/webix-jet/part-ii-webix-jet-in-details/asynchronous-views#data.waitdata

--- sources/views/data.js
+++ sources/views/data.js
@@ -1,11 +1,21 @@
-import {JetView} from "webix-jet";
 import {data} from "models/records";
+import {JetView} from "webix-jet";
+
 
 export default class DataView extends JetView{
 	config(){
-		return { view:"datatable", autoConfig:true, css:"webix_shadow_medium" };
+		return { view:"datatable", autoConfig:true };
 	}
 	init(view){
-		view.parse(data);
+		view.sync(data);
+		data.waitData.then(() => {
+			view.select(view.getFirstId());
+		});
 	}
+	//  BELOW is syncronous only, from previous example
+	// init(view){
+	// 	view.parse(data);
+	// 	const table = view as webix.ui.datatable;
+	// 	table.add({});
+	// }
 }
